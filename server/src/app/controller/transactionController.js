099f2ca72e2ced893986b81d65412da4ae4f718e
dodangquyen22 <quyend323@gmail.com>
2023-11-03 17:40:40 +0700

    connect and format

---
+++ server/src/app/controller/transactionController.js
@@ -0,0 +1,22 @@
+class transactionController{
+    //Tao don hang cho khach hang
+    createOrder(req, res, next) {
+
+    }
+    //tao don den diem tap ket
+    createDeliveryRequest(req, res,next) {
+
+    }
+    //xac nhan don hang ve tu diem tap ket
+    confirmDeliveryGather(req, res, next) {
+
+    }
+    //xac nhan don hang da chuyen den nguoi nhan
+    confirm(req, res, nexe){
+
+    }
+    //thong ke
+    statistic(req, res, next) {
+        
+    }
+}
